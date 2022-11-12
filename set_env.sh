0b444dfece7bcfdf390f35f7c453ea743e2ec25a
Alejandro Deulofeu Fleites <alejandro.deulofeu@towesoft.net>
2022-11-12 17:27:51 +0700

    export envs

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+export POSTGRES_DB = adfudagramproject
+export POSTGRES_HOST = adf-udagram-project.cn5kint2rp5k.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME = postgres
+export POSTGRES_PASSWORD = adf-udagram-project
+export DB_DIALECT = postgres
+export AWS_REGION = us-east-1
+export AWS_PROFILE = personal
+export AWS_BUCKET_NAME = adf-udagram-project
+export JWT_SECRET=asdqwe123
