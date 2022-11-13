fed19b7e141f91ca3214d1d1e0af8f4dc8bfd50f
Alejandro Deulofeu Fleites <alejandro.deulofeu@towesoft.net>
2022-11-13 12:42:21 +0700

    fix deployments and update frontend to use cluster ip

--- scripts/create_eks.sh
+++ scripts/create_eks.sh
@@ -19,10 +19,10 @@ eksctl create cluster \
     --version 1.22 \
     --region us-east-1 \
     --nodegroup-name $ENV_NAME-personal-nodegroup \
-    --node-type t2.micro \
-    --nodes 1 \
-    --nodes-min 1 \
-    --nodes-max 1 \
+    --node-type m5.large \
+    --nodes 2 \
+    --nodes-min 2 \
+    --nodes-max 3 \
     --ssh-access \
     --node-volume-size 10 \
     --ssh-public-key Default_Access_Key \
@@ -37,17 +37,17 @@ eksctl create cluster \
     sed -i .bak -e 's/v1alpha1/v1beta1/' C:\Users\adf\.kube\.kube\config
 
     ## Test kube config
-    kubectl get pods
+    # kubectl get pods
 
     ## Deploy resources and config services
-    kubetctl apply -f ./deploy/deployment.yaml
-    kubetctl apply -f ./deploy/service.yaml
+    # kubetctl apply -f ./deploy/deployment.yaml
+    # kubetctl apply -f ./deploy/service.yaml
 
 
-    ## Add autoscale configs and apply them:
-    # kubectl autoscale deployment <NAME> --cpu-percent=<CPU_PERCENTAGE> --min=<MIN_REPLICAS> --max=<MAX_REPLICAS>
-    kubectl autoscale deployment my-app-2 --cpu-percent=85  --min=5 --max=3
-    kubectl get hpa
+    # ## Add autoscale configs and apply them:
+    # # kubectl autoscale deployment <NAME> --cpu-percent=<CPU_PERCENTAGE> --min=<MIN_REPLICAS> --max=<MAX_REPLICAS>
+    # kubectl autoscale deployment my-app-2 --cpu-percent=85  --min=5 --max=3
+    # kubectl get hpa
 
 
 
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad8a2198edfaf4c7db4214b4ee4a0112-1318323971.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
